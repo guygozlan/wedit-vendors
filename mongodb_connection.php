93296a203a4d88b0f58fa5c84966e5d11fa09926
Gozlan <gozlang1@gmail.com>
2017-10-14 23:02:53 +0300

    no message

--- mongodb_connection.php
+++ mongodb_connection.php
@@ -27,13 +27,13 @@ function connect_mongodb()
     $mssg = $mssg . '<p style="color:red;"> * Mongo Exeption: </p> <br />';
     $mssg = $mssg . '<p style="color:red;">' . $e->getMessage() . '</p> <br />';
   }
-  // echo '
-  //   <div class="alert alert-warning alert-dismissible" role="alert">
-  //     <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-  //     <span aria-hidden="true">&times;</span></button>
-  //     <strong>MongoDB info: </strong> ' . $mssg . '
-  //   </div>
-  // ';
+  echo '
+    <div class="alert alert-warning alert-dismissible" role="alert">
+      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+      <span aria-hidden="true">&times;</span></button>
+      <strong>MongoDB info: </strong> ' . $mssg . '
+    </div>
+  ';
   return ($collection);
 }
 ?>
